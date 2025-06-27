0c2c6395c3e9677747f98603913e0ebb9ae1b056
G <geraldineaustinsemail@gmail.com>
2025-06-27 23:04:28 +1200

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'babel-plugin-transform-import-meta', // ✅ Put this FIRST
+      'babel-plugin-transform-import-meta', // ✅ Must be first
       [
         'module-resolver',
         {
