664ac614a63528db678627a2364cbd65e85b0150
G <geraldineaustinsemail@gmail.com>
2025-06-27 22:36:45 +1200

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,18 +4,17 @@ const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const config = getDefaultConfig(__dirname);
 
-// Add .mjs to supported source extensions
+// Add .mjs support for ESM modules
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs'];
 
-// Block problematic ESM-only or import.meta packages
+// Block only known problematic packages that must be excluded
 config.resolver.blockList = exclusionList([
   /node_modules\/@react-native\/debugger-frontend\/.*/,
   /node_modules\/acorn\/.*/,
   /node_modules\/terser\/.*/,
-  /node_modules\/glob\/dist\/esm\/.*/,
+  // ❌ Removed: /node_modules\/glob\/dist\/esm\/.*/,
   /node_modules\/lightningcss\/.*/,
   /node_modules\/sucrase\/dist\/esm\/.*/,
 ]);
 
 module.exports = config;
-
