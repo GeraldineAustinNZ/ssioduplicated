5d19f494b998e55831fd93dbc88274d5fe69664e
G <geraldineaustinsemail@gmail.com>
2025-06-27 23:09:14 +1200

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,14 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'babel-plugin-transform-import-meta', // ✅ Put this FIRST
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@': './',
-          },
-        },
-      ],
-    ],
-  };
-};
+const { getDefaultConfig } = require('expo/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+const config = getDefaultConfig(__dirname);
+
+// Do not block ESM dependencies like glob, sucrase, terser
+config.resolver.blockList = exclusionList([
+  /node_modules\/@react-native\/debugger-frontend\/.*/, // optional: noisy in debug
+]);
+
+// Ensure `.mjs` files are treated as modules and transpiled
+config.resolver.sourceExts.push('mjs');
+
+module.exports = config;
