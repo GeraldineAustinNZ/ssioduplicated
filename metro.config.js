4fae80f917c76ff0507bfaec8d6a33e2f90dcae3
G <geraldineaustinsemail@gmail.com>
2025-06-27 23:55:10 +1200

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,9 @@
 // metro.config.js
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Make sure we're not blocking ESM packages needed on web
-delete config.resolver.blockList;
-
-// Explicitly disable Hermes transforms for Web
-config.transformer = {
-  ...config.transformer,
-  unstable_disableES6Transforms: true,
-  getTransformOptions: async () => ({
-    transform: {
-      experimentalImportSupport: false,
-      inlineRequires: true,
-    },
-  }),
-};
+// Optionally enable `cjs` resolution for web safety
+config.resolver.resolveRequest = undefined;
 
 module.exports = config;
