e56be27ca304c420904e08c68653d9526f21e63a
G <geraldineaustinsemail@gmail.com>
2025-06-27 21:38:36 +1200

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,16 @@
-// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // Enables absolute imports using @/path
       [
         'module-resolver',
         {
-          root: ['./'],
           alias: {
             '@': './',
           },
-          extensions: ['.js', '.jsx', '.ts', '.tsx'],
         },
       ],
-      // Required for Expo Router support
-      'expo-router/babel',
     ],
   };
 };
