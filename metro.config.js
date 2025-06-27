8074f4b7459c64b356f6cdc8f703114aa435dd50
G <geraldineaustinsemail@gmail.com>
2025-06-27 22:08:08 +1200

    Updated metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+// metro.config.js
+const { getDefaultConfig } = require('expo/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+const config = getDefaultConfig(__dirname);
+
+// Block packages using import.meta or ESM-only builds
+config.resolver.blockList = exclusionList([
+  /node_modules\/@react-native\/debugger-frontend\/.*/,
+  /node_modules\/acorn\/.*/,
+  /node_modules\/terser\/.*/,
+  /node_modules\/glob\/dist\/esm\/.*/,
+  /node_modules\/lightningcss\/.*/,
+  /node_modules\/sucrase\/dist\/esm\/.*/,
+]);
+
+module.exports = config;
