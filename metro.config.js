b7571bf7d587072d1158159b1778834d33cb9383
G <geraldineaustinsemail@gmail.com>
2025-06-27 23:35:35 +1200

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,21 @@
 // metro.config.js
-
 const { getDefaultConfig } = require('expo/metro-config');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const config = getDefaultConfig(__dirname);
 
-// ✅ Only block known problematic packages using untranspiled import.meta
-config.resolver.blockList = exclusionList([
-  /node_modules\/@react-native\/debugger-frontend\/.*/,
-  /node_modules\/acorn\/.*/,
-  /node_modules\/terser\/.*/,
-  /node_modules\/lightningcss\/.*/,
-]);
+// Make sure we're not blocking ESM packages needed on web
+delete config.resolver.blockList;
 
-// ✅ Ensure default transformer config remains
-config.transformer.getTransformOptions = async () => ({
-  transform: {
-    experimentalImportSupport: false,
-    inlineRequires: true,
-  },
-});
+// Explicitly disable Hermes transforms for Web
+config.transformer = {
+  ...config.transformer,
+  unstable_disableES6Transforms: true,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+};
 
 module.exports = config;
