304d07ead9cccc68db4985d244a30036b3dea107
G <geraldineaustinsemail@gmail.com>
2025-06-28 09:12:55 +1200

    Updated find-import-meta.js

---
+++ scripts/find-import-meta.js
@@ -0,0 +1,16 @@
+const fg = require('fast-glob');
+const fs = require('fs');
+
+(async () => {
+  const files = await fg('node_modules/**/*.{js,mjs}', { absolute: true });
+  for (const file of files) {
+    try {
+      const content = fs.readFileSync(file, 'utf-8');
+      if (content.includes('import.meta')) {
+        console.log(file);
+      }
+    } catch (err) {
+      // Ignore unreadable files
+    }
+  }
+})();
