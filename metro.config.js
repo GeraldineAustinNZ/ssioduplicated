43c7a64f5a4f0811307b21c6721b9464262e6dc5
G <geraldineaustinsemail@gmail.com>
2025-06-27 23:22:40 +1200

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -5,8 +5,7 @@ const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const config = getDefaultConfig(__dirname);
 
-// ✅ Allow important ESM packages like `glob` to be transpiled
-// Only block problematic non-transpiled dependencies that use import.meta and don't work on web
+// ✅ Only block known problematic packages using untranspiled import.meta
 config.resolver.blockList = exclusionList([
   /node_modules\/@react-native\/debugger-frontend\/.*/,
   /node_modules\/acorn\/.*/,
@@ -14,16 +13,12 @@ config.resolver.blockList = exclusionList([
   /node_modules\/lightningcss\/.*/,
 ]);
 
-// ✅ Ensure Metro uses babel-transformer for correct syntax support
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
-  getTransformOptions: async () => ({
-    transform: {
-      experimentalImportSupport: false,
-      inlineRequires: true,
-    },
-  }),
-};
+// ✅ Ensure default transformer config remains
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
+  },
+});
 
 module.exports = config;
